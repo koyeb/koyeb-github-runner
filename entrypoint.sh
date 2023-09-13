8d5707931f11fb8472c0b6764ae5b8f668d79743
Julien Castets <castets.j@gmail.com>
2023-09-13 15:45:34 +0200

    Add environment variable EPHEMERAL

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,15 +14,23 @@ for required_env in \
     fi
 done
 
+# If the environment variable EPHEMERAL is not empty, configure the runner to
+# only take one job and then let the service un-configure the runner after the
+# job finishes
+if [ ! -z "$EPHEMERAL" ];
+then
+    ephemeral="--ephemeral"
+fi
+
 ./config.sh \
     --url "$REPO_URL" \
     --token "$GITHUB_TOKEN" \
-    --ephemeral \
     --name "koyeb-runner-$(hostname)" \
     --runnergroup default \
     --no-default-labels \
     --labels "$RUNNER_LABELS" \
     --work workspace \
-    --replace
+    --replace \
+    $ephemeral
 
 exec ./run.sh
