9ac8c85b2d7fa537e886582c231d8ba7627bfe7f
Julien Castets <castets.j@gmail.com>
2023-10-11 16:51:19 +0200

    Fix URL to retrieve token for repositories

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,7 +68,7 @@ else
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $GITHUB_TOKEN" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/repos/$REPO/actions/runners/registration-token")
+    "https://api.github.com/repos/$ORGANIZATION/$REPO/actions/runners/registration-token")
 fi
 
 RUNNER_TOKEN=$(echo $resp | jq -r .token)
